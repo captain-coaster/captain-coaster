449203e691a1c005218b5eaa8dedcf04f0aaf472
Florian Blond <florian.blond@gmail.com>
2017-06-05 19:50:52 +0200

    add basic form

--- src/BddBundle/Controller/CoasterController.php
+++ src/BddBundle/Controller/CoasterController.php
@@ -6,6 +6,10 @@ use BddBundle\Entity\Coaster;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
 
 
 /**
@@ -14,6 +18,37 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
  */
 class CoasterController extends Controller
 {
+    /**
+     * @Route("/coaster/new", name="bdd_new_coaster")
+     * @Method({"GET", "POST"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newAction(Request $request)
+    {
+        $coaster = new Coaster();
+
+        /** @var Form $form */
+        $form = $this->createFormBuilder($coaster)
+            ->add('name', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Coaster'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $coaster = $form->getData();
+        }
+
+        return $this->render(
+            'BddBundle:Coaster:new.html.twig',
+            array(
+                'form' => $form->createView(),
+            )
+        );
+    }
+
     /**
      * @Route("/coaster/{slug}", name="bdd_show_coaster")
      * @Method({"GET"})
@@ -24,11 +59,10 @@ class CoasterController extends Controller
     public function showAction(Coaster $coaster)
     {
         return $this->render(
-            'BddBundle:Coaster:coaster.html.twig',
+            'BddBundle:Coaster:show.html.twig',
             array(
                 'coaster' => $coaster,
             )
         );
     }
-
 }
