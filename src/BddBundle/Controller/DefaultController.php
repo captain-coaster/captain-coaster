e990c0e3a40f08267ce63b37d1f054b89bb2938e
Florian Blond <florian.blond@gmail.com>
2017-08-07 14:34:19 +0200

    add contact form

--- src/BddBundle/Controller/DefaultController.php
+++ src/BddBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace BddBundle\Controller;
 
+use BddBundle\Form\Type\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -14,6 +16,8 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
+     * Root of application, redirect to /en or /fr
+     *
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
@@ -83,6 +87,47 @@ class DefaultController extends Controller
                 'ratingNumber' => $ratingNumber,
                 'newRatingNumber' => $newRatingNumber,
                 'reviews' => $reviews,
+            ]
+        );
+    }
+
+    /**
+     * Contact form
+     *
+     * @param Request $request
+     * @param \Swift_Mailer $mailer
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/contact", name="default_contact")
+     * @Method({"GET", "POST"})
+     */
+    public function contactAction(Request $request, \Swift_Mailer $mailer)
+    {
+        /** @var Form $form */
+        $form = $this->createForm(ContactType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $message = (new \Swift_Message($this->get('translator')->trans('contact.email.title')))
+                ->setFrom($this->getParameter('mail_from'))
+                ->setTo($this->getParameter('mail_to'))
+                ->setReplyTo($data['email'])
+                ->setBody($data['name']."\n".$data['message']);
+            $mailer->send($message);
+
+            $this->addFlash(
+                'success',
+                $this->get('translator')->trans('contact.flash.success', ['%name%' => $data['name']])
+            );
+
+            return $this->redirectToRoute('default_contact');
+        }
+
+        return $this->render(
+            'BddBundle:Default:contact.html.twig',
+            [
+                'form' => $form->createView(),
             ]
         );
     }
---
+++ src/BddBundle/Form/Type/ContactType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace BddBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Class ContactType
+ * @package BddBundle\Form\Type
+ */
+class ContactType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'required' => true,
+                    'label' => 'contact.form.name',
+                    'translation_domain' => 'messages',
+                    'constraints' => [
+                        new NotBlank(),
+                    ],
+                ]
+            )
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'required' => false,
+                    'label' => 'contact.form.email',
+                    'translation_domain' => 'messages',
+                    'constraints' => [
+                        new Email(),
+                    ],
+                ]
+            )
+            ->add(
+                'message',
+                TextareaType::class,
+                [
+                    'required' => true,
+                    'label' => 'contact.form.message',
+                    'translation_domain' => 'messages',
+                    'constraints' => [
+                        new NotBlank(),
+                    ],
+                ]
+            );
+    }
+}
