5122690fe2854a25935b638abffaf0d7e3a7050f
Florian Blond <florian.blond@gmail.com>
2019-03-19 17:52:28 +0100

    expose API key in profile section

--- src/Form/Type/ProfileType.php
+++ src/Form/Type/ProfileType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -82,6 +83,15 @@ class ProfileType extends AbstractType
                 [
                     'required' => false,
                     'label' => 'me.form.addTodayDateWhenRating.label'
+                ]
+            )
+            ->add(
+                'apiKey',
+                TextType::class,
+                [
+                    'required' => false,
+                    'disabled' => true,
+                    'label' => 'me.form.apiKey.label'
                 ]
             );
     }
