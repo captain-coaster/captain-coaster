b8fe43f1f517469f591e17369745a92fc658cc93
Florian Blond <florian.blond@gmail.com>
2019-09-21 12:15:45 +0200

    raise levels for ranking

--- src/Service/RankingService.php
+++ src/Service/RankingService.php
@@ -19,10 +19,10 @@ class RankingService
     // Minimum comparison number between coaster A and B
     CONST MIN_COMPARISONS = 3;
     // Minimum duels for a coaster, i.e. minimum number of other coasters to be compared with
-    CONST MIN_DUELS = 250;
+    CONST MIN_DUELS = 275;
     // For elite coaster, we need more comparisons
     CONST ELITE_SCORE = 99;
-    CONST MIN_DUELS_ELITE_SCORE = 400;
+    CONST MIN_DUELS_ELITE_SCORE = 350;
 
     /**
      * @var EntityManagerInterface
@@ -255,8 +255,7 @@ class RankingService
 
                 // don't take into account if too few comparisons
                 // $comparisonResult + $reverseComparisonResult always equals vote number
-                if ($comparisonResult + $reverseComparisonResult >= self::MIN_COMPARISONS ||
-                    max($comparisonResult, $reverseComparisonResult) >= (intdiv(self::MIN_COMPARISONS, 2) + 1)) {
+                if ($comparisonResult + $reverseComparisonResult >= self::MIN_COMPARISONS) {
                     $this->totalComparisonNumber += ($comparisonResult + $reverseComparisonResult);
                     $duelCount++;
 
