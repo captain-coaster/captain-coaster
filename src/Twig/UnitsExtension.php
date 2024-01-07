31bc043169ac7eca4bd04c39bbb9d7f772059097
Richard Crété <r.crete@numendo.com>
2024-01-07 13:14:22 +0100

    Conversions in twig extension

--- src/Twig/UnitsExtension.php
+++ src/Twig/UnitsExtension.php
@@ -11,6 +11,9 @@ class UnitsExtension extends AbstractExtension
     {
         return [
             new TwigFunction('is_imperial', [$this, 'isImperial']),
+            new TwigFunction('meter_or_feet', [$this, 'm_or_f']),
+            new TwigFunction('meter_or_feet', [$this, 'kph_or_mph']),
+            new TwigFunction('meter_or_feet', [$this, 'km_or_mil']),
         ];
     }
 
@@ -20,5 +23,32 @@ class UnitsExtension extends AbstractExtension
         // TODO User
 
         return 'en' === $locale;
+    }
+
+    public function m_or_f(bool $isImperial, int $value): string
+    {
+        if ($isImperial) {
+            return round($value * 3.281).' ft';
+        }
+
+        return $value.' m';
+    }
+
+    public function kph_or_mph(bool $isImperial, int $value): string
+    {
+        if ($isImperial) {
+            return round($value / 1.609).' mph';
+        }
+
+        return $value.' km/h';
+    }
+
+    public function km_or_mil(bool $isImperial, int $value): string
+    {
+        if ($isImperial) {
+            return round($value / 1.609).' mil';
+        }
+
+        return $value.' km';
     }
 }
