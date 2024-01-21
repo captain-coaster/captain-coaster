aad85314df0733a1528f22f0072c5d0ca7d3ebcf
Florian Blond <florian.blond@gmail.com>
2024-01-21 21:11:13 +0100

    first step: login link

--- src/Controller/ConnectController.php
+++ src/Controller/ConnectController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Repository\UserRepository;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,10 +13,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Notifier\Recipient\Recipient;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
 use Symfony\Component\Security\Http\LoginLink\LoginLinkNotification;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Controller in charge of authentication routes.
@@ -27,27 +28,45 @@ class ConnectController extends AbstractController
     use TargetPathTrait;
 
     /** Display login page */
-    #[Route(path: '/login', name: 'login', methods: ['GET'])]
-    public function login(Request $request): Response
-    {
-        // save referer to redirect after login
-        $referer = $request->headers->get('referer');
-        if ($referer) {
-            $this->saveTargetPath(
-                $request->getSession(),
-                'main',
-                parse_url($referer, \PHP_URL_PATH).'?'.parse_url($referer, \PHP_URL_QUERY)
+    #[Route(path: '/login', name: 'login', methods: ['GET', 'POST'])]
+    public function login(
+        Request $request,
+        AuthenticationUtils $authenticationUtils,
+        NotifierInterface $notifier,
+        LoginLinkHandlerInterface $loginLinkHandler,
+        UserRepository $userRepository,
+        TranslatorInterface $translator,
+    ): Response {
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+            $user = $userRepository->findOneBy(['email' => $email]);
+
+            $notification = new LoginLinkNotification(
+                $loginLinkHandler->createLoginLink($user),
+                $translator->trans('login.email.title'),
+                ['email']
             );
+            $notifier->send($notification, new Recipient($user->getEmail()));
+
+            $this->addFlash('success', $translator->trans('login.link_sent', ['email' => $email]));
+        } else {
+            // save referer to redirect after login
+            $referer = $request->headers->get('referer');
+            if ($referer) {
+                $this->saveTargetPath(
+                    $request->getSession(),
+                    'main',
+                    parse_url($referer, \PHP_URL_PATH).'?'.parse_url($referer, \PHP_URL_QUERY)
+                );
+            }
+
+            // save current locale in session
+            $request->getSession()->set('locale_at_login', $request->getLocale());
         }
 
-        // save current locale in session
-        $request->getSession()->set('locale_at_login', $request->getLocale());
-
-        return $this->render('connect/login.html.twig');
-    }
-
-    public function loginCheck(): void
-    {
+        return $this->render('connect/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
     }
 
     /** Route handled in routes.yaml (no locale) */
@@ -79,34 +98,5 @@ class ConnectController extends AbstractController
     public function connectFacebookCheck(Request $request): void
     {
         // left blank as it is handled inside FacebookAuthenticator
-    }
-
-    #[Route('/login/link/start', name: 'login_link_start')]
-    // deny access for now
-    #[IsGranted('ROLE_PREVIEW_FEATURE')]
-    public function requestLoginLink(NotifierInterface $notifier, LoginLinkHandlerInterface $loginLinkHandler, UserRepository $userRepository, Request $request): Response
-    {
-        if ($request->isMethod('POST')) {
-            $email = $request->request->get('email');
-            $user = $userRepository->findOneBy(['email' => $email]);
-
-            $loginLinkDetails = $loginLinkHandler->createLoginLink($user);
-
-            // create a notification based on the login link details
-            $notification = new LoginLinkNotification(
-                $loginLinkDetails,
-                'Welcome to MY WEBSITE!' // email subject
-            );
-            // create a recipient for this user
-            $recipient = new Recipient($user->getEmail());
-
-            // send the notification to the user
-            $notifier->send($notification, $recipient);
-
-            // render a "Login link is sent!" page
-            return $this->render('connect/login.html.twig');
-        }
-
-        return $this->render('connect/login.html.twig');
     }
 }
