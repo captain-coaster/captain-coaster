b341b1cc01b0c9ff757a8607b6a9a02f35c240c3
Florian Blond <florian.blond@gmail.com>
2024-11-08 10:48:42 +0100

    add api key to easy admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -74,6 +74,7 @@ class UserCrudController extends AbstractCrudController
             BooleanField::new('enabled'),
             BooleanField::new('emailNotification')->hideOnIndex(),
             BooleanField::new('addTodayDateWhenRating')->hideOnIndex(),
+            TextField::new('apiKey')->hideOnIndex()->setFormTypeOption('disabled', 'disabled'),
             DateTimeField::new('lastLogin')->setFormTypeOption('disabled', 'disabled'),
             DateTimeField::new('createdAt')->hideOnIndex()->setFormTypeOption('disabled', 'disabled'),
             DateTimeField::new('updatedAt')->hideOnIndex()->setFormTypeOption('disabled', 'disabled'),
