2d397ccbd2db9aae41ed19283499d7d290627595
richardcrete <crete.richard.ritchi@gmail.com>
2025-04-05 01:41:59 +0200

    [#168] - Change parks URL format

--- src/Controller/ParkController.php
+++ src/Controller/ParkController.php
@@ -8,13 +8,22 @@ use App\Entity\Park;
 use App\Repository\CoasterRepository;
 use App\Repository\ParkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[\Symfony\Component\Routing\Attribute\Route(path: '/parks')]
 class ParkController extends AbstractController
 {
+    /** Redirects to index */
+    #[\Symfony\Component\Routing\Attribute\Route(path: '/', name: 'park_index', methods: ['GET'])]
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('bdd_index');
+    }
+
     /** Show park details. */
-    #[Route(path: '/parks/{slug}', name: 'park_show', options: ['expose' => true], methods: ['GET'])]
+    #[Route(path: '/{id}/{slug}', name: 'park_show', options: ['expose' => true], methods: ['GET'])]
     public function showAction(
         ParkRepository $parkRepository,
         Park $park,
@@ -28,5 +37,15 @@ class ParkController extends AbstractController
                 'closestParks' => $parkRepository->getClosestParks($park, 80, 300),
             ]
         );
+    }
+
+    /** Redirect old urls to above */
+    #[\Symfony\Component\Routing\Attribute\Route(path: '/{slug}', name: 'redirect_park_show', options: ['expose' => true], methods: ['GET'])]
+    public function redirectCoaster(Park $park): RedirectResponse
+    {
+        return $this->redirectToRoute('park_show', [
+            'id' => $park->getId(),
+            'slug' => $park->getSlug(),
+        ], 301);
     }
 }
