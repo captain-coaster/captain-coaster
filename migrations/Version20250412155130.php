8c2b41e1e5a2c62bb3a1cf5a3b8cbe05d5c5b75e
richardcrete <crete.richard.ritchi@gmail.com>
2025-04-12 18:25:07 +0200

    [#145+#186] - CS Fixer

--- migrations/Version20250412155130.php
+++ migrations/Version20250412155130.php
@@ -21,21 +21,21 @@ final class Version20250412155130 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE continent_translation (id INT AUTO_INCREMENT NOT NULL, translatable_id INT NOT NULL, name VARCHAR(255) NOT NULL, locale VARCHAR(255) NOT NULL, INDEX IDX_E1BACE4B2C2AC5D3 (translatable_id), UNIQUE INDEX continent_translation_uniq_trans (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
-        SQL);
+                CREATE TABLE continent_translation (id INT AUTO_INCREMENT NOT NULL, translatable_id INT NOT NULL, name VARCHAR(255) NOT NULL, locale VARCHAR(255) NOT NULL, INDEX IDX_E1BACE4B2C2AC5D3 (translatable_id), UNIQUE INDEX continent_translation_uniq_trans (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            SQL);
         $this->addSql(<<<'SQL'
-            ALTER TABLE continent_translation ADD CONSTRAINT FK_E1BACE4B2C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES continent (id) ON DELETE CASCADE
-        SQL);
+                ALTER TABLE continent_translation ADD CONSTRAINT FK_E1BACE4B2C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES continent (id) ON DELETE CASCADE
+            SQL);
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE continent_translation DROP FOREIGN KEY FK_E1BACE4B2C2AC5D3
-        SQL);
+                ALTER TABLE continent_translation DROP FOREIGN KEY FK_E1BACE4B2C2AC5D3
+            SQL);
         $this->addSql(<<<'SQL'
-            DROP TABLE continent_translation
-        SQL);
+                DROP TABLE continent_translation
+            SQL);
     }
 }
