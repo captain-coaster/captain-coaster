9db2ecb0c2c39552d4a4014026e7419b993b3472
Florian Blond <florian.blond@gmail.com>
2025-04-16 09:57:58 +0200

    php cs fixer

--- migrations/Version20250413101315.php
+++ migrations/Version20250413101315.php
@@ -21,15 +21,15 @@ final class Version20250413101315 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE users ADD name_changed_at DATETIME DEFAULT NULL, ADD display_name_format VARCHAR(20) DEFAULT 'full' NOT NULL
-        SQL);
+                ALTER TABLE users ADD name_changed_at DATETIME DEFAULT NULL, ADD display_name_format VARCHAR(20) DEFAULT 'full' NOT NULL
+            SQL);
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE users DROP name_changed_at, DROP display_name_format
-        SQL);
+                ALTER TABLE users DROP name_changed_at, DROP display_name_format
+            SQL);
     }
 }
