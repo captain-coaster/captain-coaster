80639450a7e8b8391c62015a78b4a6559472bc1a
richardcrete <crete.richard.ritchi@gmail.com>
2025-04-30 19:34:40 +0200

    [#51] - Change Search controller & templates to Nearby

--- src/Controller/NearbyController.php
+++ src/Controller/NearbyController.php
@@ -6,19 +6,15 @@ namespace App\Controller;
 
 use App\Repository\CoasterRepository;
 use App\Repository\ManufacturerRepository;
-use App\Service\SearchService;
 use Knp\Component\Pager\PaginatorInterface;
-use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(path: '/search')]
-class SearchController extends AbstractController
+#[Route(path: '/nearby')]
+class NearbyController extends AbstractController
 {
     final public const string CACHE_AUTOCOMPLETE = 'main_autocomplete';
 
@@ -29,11 +25,11 @@ class SearchController extends AbstractController
         $this->paginator = $paginator;
     }
 
-    #[Route(path: '/', name: 'search_index', methods: ['GET'])]
+    #[Route(path: '/', name: 'nearby_index', methods: ['GET'])]
     public function indexAction(ManufacturerRepository $manufacturerRepository, CoasterRepository $coasterRepository): Response
     {
         return $this->render(
-            'Search/index.html.twig',
+            'Nearby/index.html.twig',
             [
                 'filters' => ['status' => 'on'],
                 'filtersForm' => [
@@ -44,7 +40,7 @@ class SearchController extends AbstractController
         );
     }
 
-    #[Route(path: '/coasters', name: 'search_coasters_ajax', options: ['expose' => true], methods: ['GET'], condition: 'request.isXmlHttpRequest()')]
+    #[Route(path: '/coasters', name: 'nearby_coasters_ajax', options: ['expose' => true], methods: ['GET'], condition: 'request.isXmlHttpRequest()')]
     public function searchAction(
         CoasterRepository $coasterRepository,
         #[MapQueryParameter]
@@ -63,32 +59,6 @@ class SearchController extends AbstractController
             throw new BadRequestHttpException();
         }
 
-        return $this->render('Search/results.html.twig', ['coasters' => $pagination]);
-    }
-
-    /**
-     * All data for main search service.
-     *
-     * @throws InvalidArgumentException
-     */
-    #[Route(path: '/main.json', name: 'ajax_main_search', options: ['expose' => true], methods: ['GET'], condition: 'request.isXmlHttpRequest()')]
-    public function ajaxMainSearch(SearchService $searchService): JsonResponse
-    {
-        $cache = new FilesystemAdapter();
-        $searchItems = $cache->getItem(self::CACHE_AUTOCOMPLETE);
-
-        if (!$searchItems->isHit()) {
-            $searchItems->set($searchService->getAutocompleteValues());
-            $searchItems->expiresAfter(\DateInterval::createFromDateString('24 hours'));
-            $cache->save($searchItems);
-        }
-
-        $response = new JsonResponse($searchItems->get());
-        $response->setEncodingOptions(\JSON_UNESCAPED_UNICODE);
-
-        $response->setPublic();
-        $response->setMaxAge(600);
-
-        return $response;
+        return $this->render('Nearby/results.html.twig', ['coasters' => $pagination]);
     }
 }
