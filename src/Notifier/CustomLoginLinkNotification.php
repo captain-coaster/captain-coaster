d9f12a4747260740cf39de1025aaa56ca61300f6
Florian Blond <florian.blond@gmail.com>
2025-05-08 16:55:39 +0200

    upgrade project setup

--- src/Notifier/CustomLoginLinkNotification.php
+++ src/Notifier/CustomLoginLinkNotification.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Notifier;
 
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
 use Symfony\Component\Notifier\Message\EmailMessage;
 use Symfony\Component\Notifier\Recipient\EmailRecipientInterface;
 use Symfony\Component\Security\Http\LoginLink\LoginLinkNotification;
@@ -14,7 +15,11 @@ class CustomLoginLinkNotification extends LoginLinkNotification
     {
         $emailMessage = parent::asEmailMessage($recipient, $transport);
 
-        // get the NotificationEmail object and override the template
+        /**
+         * get the NotificationEmail object and override the template.
+         *
+         * @var NotificationEmail $email
+         */
         $email = $emailMessage->getMessage();
         $email->htmlTemplate('connect/login_email.html.twig');
 
