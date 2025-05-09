e25c956b9b427c54f7d3b45727fbf4ce01a7a21a
Florian Blond <florian.blond@gmail.com>
2025-05-09 22:53:24 +0200

    basic layout (navbar, sidebar, footer)

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
 import "./app.css";
 
 console.log("Happy coding !!");
+
+import imgUrl from "./images/logo/logo.svg";
---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+export default {
+    semi: false,
+    useTabs: false,
+    tabWidth: 4,
+    printWidth: 100,
+    trailingComma: "es5",
+    quoteProps: "preserve",
+    endOfLine: "lf",
+    twigAlwaysBreakObjects: false,
+    plugins: ["prettier-plugin-tailwindcss", "@zackad/prettier-plugin-twig"],
+}
