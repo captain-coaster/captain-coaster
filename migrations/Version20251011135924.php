974a3f995c747c140f5762e397f0ee442185ae35
richardcrete <crete.richard.ritchi@gmail.com>
2025-10-11 16:41:35 +0200

    [#228] - Add setting to Profile

---
+++ migrations/Version20251011135924.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251011135924 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users ADD hide_non_operating_coasters_in_top100_stat TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users DROP hide_non_operating_coasters_in_top100_stat');
+    }
+}
