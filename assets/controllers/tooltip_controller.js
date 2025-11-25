0c88ae3fd7d3d08b3ac848292f118e7368b6a97a
richardcrete <crete.richard.ritchi@gmail.com>
2025-11-25 11:27:20 +0100

    [#263] - Custom tooltip

--- assets/controllers/tooltip_controller.js
+++ assets/controllers/tooltip_controller.js
@@ -1,10 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static targets = ['content'];
+
     connect() {
-        $(this.element).tooltip({
-            placement: 'bottom',
-            animation: true
+        this.tooltipListeners();
+    }
+
+    tooltipListeners() {
+        this.element.addEventListener('mouseenter', () => {
+            this.contentTarget.style.display = 'block';
+        });
+
+        this.element.addEventListener('mouseleave', () => {
+            this.contentTarget.style.display = 'none';
         });
     }
 }
