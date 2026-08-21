c6a7d600d5453132887f7ed5c40bb2a1e74f2d75
Florian Blond <florian.blond@gmail.com>
2026-08-21 13:13:00 +0200

    feat: add coaster factory and fixtures

---
+++ src/Factory/CoasterFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Factory;
+
+use App\Entity\Coaster;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/** @extends PersistentProxyObjectFactory<Coaster> */
+final class CoasterFactory extends PersistentProxyObjectFactory
+{
+    public static function class(): string
+    {
+        return Coaster::class;
+    }
+
+    protected function defaults(): array
+    {
+        return [
+            'name' => self::faker()->unique()->word(),
+            'park' => ParkFactory::random(),
+            'materialType' => MaterialTypeFactory::random(),
+            'seatingType' => SeatingTypeFactory::random(),
+            'manufacturer' => ManufacturerFactory::random(),
+            'restraint' => RestraintFactory::random(),
+            'status' => StatusFactory::repository()->findOneBy(['name' => 'Operating']),
+            'openingDate' => self::faker()->dateTimeBetween('-40 years', '-1 year'),
+            'height' => self::faker()->numberBetween(20, 140),
+            'speed' => self::faker()->numberBetween(40, 150),
+            'length' => self::faker()->numberBetween(500, 2000),
+            'inversionsNumber' => self::faker()->numberBetween(0, 4),
+            'enabled' => true,
+        ];
+    }
+}
